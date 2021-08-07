4a88e23e418e76a6059feae28ccd3964a40560d2
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2021-08-07 08:29:29 -0400

    final changes and i am leaving

--- javascript.js
+++ javascript.js
@@ -7,19 +7,19 @@ function showCase() {
     }
     function display() {
         var heart_rt = document.getElementById("hrate").value;
-        document.getElementById("heart_rate").innerHTML = heart_rt;
+        document.getElementById("heart_rate").innerHTML = heart_rt + "BPM";
 
         var temp = document.getElementById("temperature").value;
-        document.getElementById("temp").innerHTML = temp;
+        document.getElementById("temp").innerHTML = temp + "C";
 
         var pulse = document.getElementById("pulse").value;
-        document.getElementById("pulse_rate").innerHTML = pulse;
+        document.getElementById("pulse_rate").innerHTML = pulse + "BPM";
 
         var respr = document.getElementById("respr").value;
-        document.getElementById("respr_rate").innerHTML = respr;
+        document.getElementById("respr_rate").innerHTML = respr + "PPT";
 
         var blood_pressure = document.getElementById("bl_pressure").value;
-        document.getElementById("pressure").innerHTML = blood_pressure;
+        document.getElementById("pressure").innerHTML = blood_pressure + "mm Hg";
     }
     display();
 }
